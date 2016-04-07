ac1fcfee328ab421466403feada46727c9416d33
ramirouvalle <ramiro.uvallevgr@uanl.edu.mx>
2016-04-07 17:45:29 -0500

    Actualizacion 6.4 Se realizaron algunos cambios en la forma de como se mostraran los ultimos rides en la pagina principal y cambios de diseño leves.

--- new_rides.php
+++ new_rides.php
@@ -9,13 +9,32 @@
 	$routesJSON = json_encode($to_encode);
 	$routes = json_decode($routesJSON);
 	
-	echo "<ul>";
+	//echo "<ul>";
 	for ($i=0; $i < count($routes) ; $i++) { 
 		$route = $routes[$i];	
 		for ($j=0; $j < count($route) ; $j++) { 
 			$nom_ruta = $route->nom_ruta;
-			 echo "<a href='#'><li><h4>".$nom_ruta."</h4></li></a>";
+			$destino_x = $route->destino_x;
+			$destino_y = $route->destino_y;
+			
+			if($destino_x == 25.7238862 && $destino_y == -100.31285739999998){
+				$destino = "Ciudad Universitaria";
+				$destinoCSS = "universidad";
+			}else if($destino_x == 25.61415077 && $destino_y == -100.28184055999998){
+				$destino = "Mederos";
+				$destinoCSS = "mederos";
+			}else if($destino_x == 25.69185015 && $destino_y == -100.34884213999999){
+				$destino = "Hospital";
+				$destinoCSS = "hospital";
+			}
+
+			echo "<div class='ride'>".
+			 	"<a href='#'>".
+			 		"<h4>".$nom_ruta."</h4>".
+			 	"</a>".
+			 	"<div class='destino ".$destinoCSS."'>".$destino."</div>".
+			 	"</div>";
 		}
 	}
-	echo "</ul>";
+	//echo "</ul>";
 ?>
